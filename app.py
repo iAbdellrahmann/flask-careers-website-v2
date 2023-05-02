9e2d08de2e977eba84802ed084b353b2673caf14
Abdelrahman Moustafa <39285876+iAbdellrahmann@users.noreply.github.com>
2023-05-02 00:16:01 +0000

    job pages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,jsonify
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 
 
 app = Flask(__name__)
@@ -11,13 +11,23 @@ def hello_jovian():
     jobs = load_jobs_from_db()
     return render_template('home.html', 
                            jobs=jobs, 
-                           company_name='Jovian')
+                           company_name='Agency')
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
+@app.route("/job/<id>")
+def show_job(id):
+   job = load_job_from_db(id)
+   if not job:
+      return "Not Found", 404
+   
+   return render_template('jobpage.html',job=job)
+   
+   
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -21,3 +21,13 @@ def load_jobs_from_db():
     jobs = [dict(zip(result.keys(), row)) for row in result_all]
     return jobs
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from jobs WHERE id ={id}"))
+    rows = result.fetchall()
+
+    if len(rows) == 0:
+      return None
+    else :
+      return rows[0]._asdict()
